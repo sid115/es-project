c3c3fefd08bc7f9c313f376e7ffbeb2855e3b8d6
sid <sid@portuus.de>
2024-01-11 19:45:23 +0100

    prng

---
+++ src/prng/README.md
@@ -0,0 +1,25 @@
+# Pseudo Random Number Generator in C
+
+> Work in Progress
+
+This is not a library yet, but a collection of functions that can be tested in `main()`. The goal is to refine these functions and implement them in a proper library.
+
+## Compile
+
+> Tested on Linux only! This probably won't work on Windows.
+
+Use the [Makefile](./Makefile) to compile:
+
+```bash
+make
+```
+
+## Execute
+
+> Tested on Linux only! This probably won't work on Windows.
+
+Run: 
+
+```bash
+./prng <whatever your main needs>
+```
---
+++ src/prng/prng.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "numbers.c" /* array of pseudo random numbers */
+
+/* saves PRNG state */
+typedef struct {
+    uint* numbers;  /* pointer to array of pre-generated random numbers */
+    uint index;     /* current index in array */
+    uint size;
+} PseudoRNG;
+
+void initPRNG();
+uint getRand();
+
+/* use main for testing */
+int main(int argc, char *argv[]) {
+    if (argc != 2) exit(1);
+
+    uint numNums   = atoi(argv[1]);
+
+    PseudoRNG prng;
+
+    initPRNG(&prng, numbers, SIZE_NUMBERS);
+    for (uint i = 0; i < numNums; i++) {
+        printf("%d: %d % 4 = %d\n", i, prng.numbers[i], prng.numbers[i] % 4);
+    }
+
+    return 0;
+}
+
+/* init PRNG with a pre-generated list of random numbers */
+void initPRNG(PseudoRNG* rng, uint* numbers, uint size) {
+    rng->numbers = numbers;
+    rng->index = 0;
+    rng->size = size;
+}
+
+/* get next random number */
+uint getRand(PseudoRNG *rng) {
+    uint number = rng->numbers[rng->index];
+    rng->index = (rng->index + 1) % rng->size;
+    return number;
+}
